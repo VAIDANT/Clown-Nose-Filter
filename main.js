16f7d1862497299d4ee788b413c1c48b2090f058
VAIDANT <72136359+VAIDANT@users.noreply.github.com>
2021-07-22 14:30:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+nox=0;
+noy=0;
+function preload() {
+    img = loadImage("nose.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Loaded');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(img, nox, noy, 30, 30)
+}
+
+function take_snapshot() {
+    save('FilteredImage.jpg')
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        nox = results[0].pose.nose.x + 15;
+        noy = results[0].pose.nose.y + 15;
+        console.log("nose x = " + nox);
+        console.log("nose y = " + noy);
+    }
+}
